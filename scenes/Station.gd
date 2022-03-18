0a6b873c7f53aedd0b495cb9024857667d5e1b11
Élise Rubio <elise.rubio@4sh.fr>
2022-03-18 18:36:52 +0100

    Load station picture depending on type

--- scenes/Station.gd
+++ scenes/Station.gd
@@ -2,11 +2,27 @@ extends Area2D
 
 signal station_selected
 
-export var type = "pouet"
+enum StationType {TYPE_1, TYPE_2, TYPE_3}
+export var type = StationType.TYPE_1
 var client = null
 
 func _ready():
-	pass
+	var station_image = pick_station_image()
+	$Sprite.texture = station_image
+	
+func pick_station_image():
+	var image_filename = ""
+	
+	match type:
+		StationType.TYPE_1:
+			image_filename = "green_square.png"
+		StationType.TYPE_2:
+			image_filename = "blue_square.png"
+		StationType.TYPE_3:
+			image_filename = "red_square.png"
+
+	return load("res://assets/" + image_filename)
+			
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
