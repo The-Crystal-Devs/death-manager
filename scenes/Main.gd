a57061476a5086d709cb9a79af3c3477abbe0317
Élise Rubio <elise.rubio@4sh.fr>
2022-03-18 17:49:53 +0100

    Register to client to retrieve their MONEY

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -8,7 +8,7 @@ var money = 0
 func _ready():
 	spawn_client()
 
-func _on_Client_client_left(money_received):
+func _on_Client_left(money_received):
 	self.money += money_received
 	emit_signal("money_changed", self.money)
 
@@ -16,4 +16,5 @@ func spawn_client():
 	var client = client_scene.instance()
 	client.position = Vector2(42, 42)
 	client.register_station(get_node("Station"))
+	client.connect("client_left", self, "_on_Client_left")
 	add_child(client)
