575f9b77a21293bef009961c8e89ed3ba354c37b
Élise Rubio <elise.rubio@4sh.fr>
2022-03-18 17:39:03 +0100

    Try to spawn client

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 signal money_changed
+export(PackedScene) var client_scene
 
 var money = 0
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _on_Client_client_left(money_received):
 	self.money += money_received
 	emit_signal("money_changed", self.money)
+
+func spawn_client():
+	var client = client_scene.instance()
+	client.position = Vector2(42, 42)
+	add_child(client)
