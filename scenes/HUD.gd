edb0dc30f1837fad559e6dfaa46bbb58bf5e7b90
Élise Rubio <elise.rubio@4sh.fr>
2022-03-20 15:28:01 +0100

    Display final score when game is over

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -1,11 +1,18 @@
 extends CanvasLayer
 
+var current_score = "0"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$GameOver.hide()
 
 func _on_Level_money_changed(money):
-	$MoneyLabel.text = "$ " + str(money)
+	current_score = str(money)
+	$MoneyLabel.text = "$ " + current_score
 
 func _on_Level_remaining_seconds_changed(remaining_seconds):
 	$TimerLabel.text = str(remaining_seconds)
+
+func _on_Level_game_over():
+	$GameOver/FinalScoreLabel.text = "Your final score: $" + current_score
+	$GameOver.show()
